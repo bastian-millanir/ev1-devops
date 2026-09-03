23fcfc1518e4dd91d5bb099a220c931499ed9999
bastian-millanir <bastian.millanir@gmail.com>
2026-09-02 20:51:59 -0400

    feat: set up basic Express server with a root route. The GET method is going to be tested by us using Postman.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Happy developing ✨')
+import express, { Request, Response } from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.json({ mensaje: 'EV 1 DevOps corriendo :-)' });
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
